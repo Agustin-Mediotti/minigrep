11b974843d2b241766d47cda229e562908f9f511
NetCreature <agustinmediotti@gmail.com>
2024-07-01 13:48:03 -0300

    stderr for error messages

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|e| {
-        println!("Problem parsing arguments: {e}");
+        eprintln!("Problem parsing arguments: {e}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
