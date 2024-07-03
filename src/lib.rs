6fbbf05c003156eea048a30785c39cd4c6739dd7
NetCreature <agustinmediotti@gmail.com>
2024-07-03 10:51:19 -0300

    using iterators on args

--- src/lib.rs
+++ src/lib.rs
@@ -46,18 +46,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        let ignore_case = if args.len() == 4 {
-            args[3].contains("-i")
-        } else {
-            env::var("IGNORE_CASE").is_ok()
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(args) => args,
+            None => return Err("Didn't get a query string"),
         };
+        let file_path = match args.next() {
+            Some(args) => args,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
             query,
